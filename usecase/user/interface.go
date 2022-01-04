f3cadf84be643007c293b8fa6c20f39f2c9ab1fd
Felipe Ferreira <me@felipefbs.dev>
2022-01-04 10:06:30 -0300

    feat: in memory repository for testing

---
+++ usecase/user/interface.go
@@ -0,0 +1,19 @@
+package user
+
+import "github.com/felipefbs/MonsterAPI/entity"
+
+type Repository interface {
+	Get(id entity.ID) (*entity.User, error)
+	GetAll() ([]*entity.User, error)
+	Store(user *entity.User) error
+	Update(user *entity.User) error
+	Delete(id entity.ID) error
+}
+
+type UseCase interface {
+	GetUser(id entity.ID) (*entity.User, error)
+	GetAllUsers() ([]*entity.User, error)
+	StoreUser(email, password, nickname string) (entity.ID, error)
+	UpdateUser(user *entity.User) error
+	DeleteUser(id entity.ID) error
+}
---
+++ usecase/user/memory.go
@@ -0,0 +1,63 @@
+package user
+
+import (
+	"errors"
+
+	"github.com/felipefbs/MonsterAPI/entity"
+	"github.com/google/uuid"
+)
+
+type memoryRepository struct {
+	database map[uuid.UUID]*entity.User
+}
+
+func NewMemoryRepository() Repository {
+	database := make(map[entity.ID]*entity.User)
+	return &memoryRepository{database}
+}
+
+func (r *memoryRepository) Get(id entity.ID) (*entity.User, error) {
+	if r.database[id] == nil {
+		return nil, errors.New("not found")
+	}
+
+	return r.database[id], nil
+}
+
+func (r *memoryRepository) GetAll() ([]*entity.User, error) {
+	users := make([]*entity.User, 0)
+
+	for _, user := range r.database {
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (r *memoryRepository) Store(user *entity.User) error {
+	r.database[user.ID] = user
+
+	return nil
+}
+
+func (r *memoryRepository) Update(user *entity.User) error {
+	_, err := r.Get(user.ID)
+	if err != nil {
+		return err
+	}
+
+	r.database[user.ID] = user
+
+	return nil
+}
+
+func (r *memoryRepository) Delete(id entity.ID) error {
+	_, err := r.Get(id)
+	if err != nil {
+		return err
+	}
+
+	r.database[id] = nil
+
+	return nil
+}
