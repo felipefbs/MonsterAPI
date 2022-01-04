f2631924a98d1d00f7a21dcb262a3c362e95560c
Felipe Ferreira <me@felipefbs.dev>
2022-01-04 11:07:30 -0300

    feat: store user function

--- usecase/user/service.go
+++ usecase/user/service.go
@@ -1,5 +1,7 @@
 package user
 
+import "github.com/felipefbs/MonsterAPI/entity"
+
 type Service struct {
 	repo Repository
 }
@@ -7,3 +9,22 @@ type Service struct {
 func NewService(repo Repository) UseCase {
 	return &Service{repo: repo}
 }
+
+func (s *Service) GetUser(id entity.ID) (*entity.User, error) { return &entity.User{}, nil }
+func (s *Service) GetAllUsers() ([]*entity.User, error)       { return []*entity.User{}, nil }
+
+func (s *Service) StoreUser(email, password, nickname string) (entity.ID, error) {
+	e, err := entity.NewUser(email, password, nickname)
+	if err != nil {
+		return entity.NilID, err
+	}
+
+	if err = s.repo.Store(e); err != nil {
+		return entity.NilID, err
+	}
+
+	return e.ID, nil
+}
+
+func (s *Service) UpdateUser(user *entity.User) error { return nil }
+func (s *Service) DeleteUser(id entity.ID) error      { return nil }
