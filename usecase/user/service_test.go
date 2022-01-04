f2ae1db0196f39832e41357a8c9ee15db501b56c
Felipe Ferreira <me@felipefbs.dev>
2022-01-04 11:33:50 -0300

    test: testing get functions

--- usecase/user/service_test.go
+++ usecase/user/service_test.go
@@ -34,3 +34,35 @@ func Test_Store(t *testing.T) {
 	_, err = service.StoreUser("", "", "")
 	assert.NotNil(t, err)
 }
+
+func Test_SearchAndFind(t *testing.T) {
+	repo := user.NewMemoryRepository()
+	service := user.NewService(repo)
+
+	u1 := newFixtureUser()
+	u2 := newFixtureUser()
+	u2.Nickname = "Bono Vox"
+
+	uID, _ := service.StoreUser(u1.Email, u1.Password, u1.Nickname)
+	_, _ = service.StoreUser(u1.Email, u1.Password, u1.Nickname)
+
+	t.Run("Get one user", func(t *testing.T) {
+		savedUser, err := service.GetUser(uID)
+
+		assert.Nil(t, err)
+		assert.Equal(t, u1.Nickname, savedUser.Nickname)
+	})
+
+	t.Run("Get an user that do not exist", func(t *testing.T) {
+		_, err := service.GetUser(entity.NilID)
+
+		assert.NotNil(t, err)
+	})
+
+	t.Run("Get all users", func(t *testing.T) {
+		all, err := service.GetAllUsers()
+
+		assert.Nil(t, err)
+		assert.Equal(t, 2, len(all))
+	})
+}
