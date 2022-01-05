da72035f2ccd56a8e22270ab13106d3d268c40ac
Felipe Ferreira <me@felipefbs.dev>
2022-01-05 17:32:47 -0300

    feat: added use case functions

--- usecase/monster/service.go
+++ usecase/monster/service.go
@@ -1,6 +1,8 @@
 package monster
 
 import (
+	"time"
+
 	"github.com/felipefbs/MonsterAPI/entity"
 )
 
@@ -12,32 +14,61 @@ func NewService(repo Repository) UseCase {
 	return &Service{repo: repo}
 }
 
-func (s *Service) GetMonsterByID(id string) (*entity.Monster, error) {
-	return nil, nil
+func (s *Service) GetMonsterByID(id entity.ID) (*entity.Monster, error) {
+	return s.repo.Get(id)
 }
 
 func (s *Service) GetMonsterByName(name string) (*entity.Monster, error) {
-	return nil, nil
+	return s.repo.GetByName(name)
 }
 
 func (s *Service) GetMonsterBySetting(setting string) ([]*entity.Monster, error) {
-	return nil, nil
+	monsters, err := s.repo.GetBySetting(setting)
+	if err != nil {
+		return nil, err
+	}
+
+	return monsters, nil
 }
 
 func (s *Service) GetMonsterByMonsterTags(tags []string) ([]*entity.Monster, error) {
-	return nil, nil
+	monsters, err := s.repo.GetByMonsterTags(tags)
+	if err != nil {
+		return nil, err
+	}
+
+	return monsters, nil
 }
 
 func (s *Service) GetMonsterByAttackTags(tags []string) ([]*entity.Monster, error) {
-	return nil, nil
+	monsters, err := s.repo.GetByAttackTags(tags)
+	if err != nil {
+		return nil, err
+	}
+
+	return monsters, nil
 }
 
 func (s *Service) GetAllMonster() ([]*entity.Monster, error) {
-	return nil, nil
+	all, err := s.repo.GetAll()
+	if err != nil {
+		return nil, err
+	}
+	if len(all) == 0 {
+		return nil, entity.ErrNotFound
+	}
+
+	return all, nil
 }
 
 func (s *Service) UpdateMonster(monster *entity.Monster) error {
-	return nil
+	if err := monster.Validate(); err != nil {
+		return err
+	}
+
+	monster.UpdatedAt = time.Now()
+
+	return s.repo.Update(monster)
 }
 
 func (s *Service) StoreMonster(
@@ -77,9 +108,19 @@ func (s *Service) StoreMonster(
 }
 
 func (s *Service) DeleteMonsterByID(id entity.ID) error {
-	return nil
+	_, err := s.repo.Get(id)
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Delete(id)
 }
 
 func (s *Service) DeleteMonsterByName(name string) error {
-	return nil
+	m, err := s.repo.GetByName(name)
+	if err != nil {
+		return err
+	}
+
+	return s.repo.Delete(m.ID)
 }
