fe85560de34b1d5e70557e76b711a2f480f46cd3
Felipe Ferreira <me@felipefbs.dev>
2022-01-05 09:54:35 -0300

    feat: monster interface and methods

---
+++ usecase/monster/interface.go
@@ -0,0 +1,26 @@
+package monster
+
+import (
+	"github.com/felipefbs/MonsterAPI/entity"
+)
+
+type Repository interface {
+	Get(id entity.ID) (*entity.Monster, error)
+	GetAll() ([]*entity.Monster, error)
+	Store(user *entity.Monster) error
+	Update(user *entity.Monster) error
+	Delete(id entity.ID) error
+}
+
+type UseCase interface {
+	GetMonsterByID(id string) (*entity.Monster, error)
+	GetMonsterByName(name string) (*entity.Monster, error)
+	GetMonsterBySetting(setting string) ([]*entity.Monster, error)
+	GetMonsterByMonsterTags(tags []string) ([]*entity.Monster, error)
+	GetMonsterByAttackTags(tags []string) ([]*entity.Monster, error)
+	GetAllMonster() ([]*entity.Monster, error)
+	UpdateMonster(monster *entity.Monster) error
+	StoreMonster(name string, moves []string, instinct string, description string, attack string, attackTags []string, damage string, monsterTags []string, hp int32, armor int32, specialQualities []string, setting string, source string) (entity.ID, error)
+	DeleteMonsterByID(id entity.ID) error
+	DeleteMonsterByName(name string) error
+}
---
+++ usecase/monster/service.go
@@ -0,0 +1,67 @@
+package monster
+
+import (
+	"github.com/felipefbs/MonsterAPI/entity"
+)
+
+type Service struct {
+	repo Repository
+}
+
+func NewService(repo Repository) UseCase {
+	return &Service{repo: repo}
+}
+
+func (s *Service) GetMonsterByID(id string) (*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) GetMonsterByName(name string) (*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) GetMonsterBySetting(setting string) ([]*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) GetMonsterByMonsterTags(tags []string) ([]*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) GetMonsterByAttackTags(tags []string) ([]*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) GetAllMonster() ([]*entity.Monster, error) {
+	return nil, nil
+}
+
+func (s *Service) UpdateMonster(monster *entity.Monster) error {
+	return nil
+}
+
+func (s *Service) StoreMonster(
+	name string,
+	moves []string,
+	instinct string,
+	description string,
+	attack string,
+	attackTags []string,
+	damage string,
+	monsterTags []string,
+	hp int32,
+	armor int32,
+	specialQualities []string,
+	setting string,
+	source string,
+) (entity.ID, error) {
+	return entity.NilID, nil
+}
+
+func (s *Service) DeleteMonsterByID(id entity.ID) error {
+	return nil
+}
+
+func (s *Service) DeleteMonsterByName(name string) error {
+	return nil
+}
